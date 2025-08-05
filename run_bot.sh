5c60db1a14b5e7bd4b7ccef2680c87a12e396b8c
AgustinMadygraf <agustin.mtto.madygraf>
2025-08-04 23:51:57 -0300

    Refactor run_bot.sh to improve environment setup and script execution

--- run_bot.sh
+++ run_bot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd /home/agustinmadygraf/RasaBot
-# cargar variables del .env
-if [ -f .env ]; then
-  set -a; . ./.env; set +a
-fi
-exec /home/agustinmadygraf/RasaBot/.venv/bin/python -u /home/agustinmadygraf/RasaBot/run.py
+set -euo pipefail
+cd ~/RasaBot
+source .venv/bin/activate
+exec python -u run.py
